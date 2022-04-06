9384c5362283b98463fe0cc5d1554a1259e33ff9
Markus Ahlstedt <yrittaja1e9@gmail.com>
2022-04-06 03:04:51 +0300

    Toka committi

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 
+function main() {
+
+}
+
+function siirry_oikea1() {
+    const element = document.getElementById("oikea1");
+    element.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+function siirry_oikea2() {
+    const element = document.getElementById("oikea2");
+    element.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+function siirry_oikea3() {
+    const element = document.getElementById("oikea3");
+    element.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+function siirry_oikea4() {
+    const element = document.getElementById("oikea4");
+    element.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+function siirry_oikea5() {
+    const element = document.getElementById("oikea5");
+    element.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
 
 
 
